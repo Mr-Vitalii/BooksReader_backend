c8dbf90efd04fc9daa27b6af6d0bf8ee3517ae97
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-07-27 10:31:40 +0300

    added database methods

--- routes/api/books.js
+++ routes/api/books.js
@@ -15,6 +15,8 @@ router.get("/:id", isValidId, ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+
 router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
 router.delete("/:id", ctrl.deleteById);
